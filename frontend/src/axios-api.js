a8c432ef535abfa505109e4a900b21d3a545af05
mohmmad-okasha <mhmmadokasha@gmail.com>
2023-06-02 16:04:14 +0300

    up

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 var currentURL = window.location.protocol + "//" + window.location.hostname + ":8000" + window.location.pathname;
 const domain_url = currentURL;
-
+ 
 //13.51.198.229
 //127.0.0.1
 const my_api = axios.create({
